4d7a0a13078b4f6be32a2a6607f909dd248a6065
hyvuz <haneennasser1425@outlook.com>
2025-03-18 00:43:45 +0000

    Functions prototype

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c); 
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int op_add(int a, int b);
