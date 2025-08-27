680dc7ebae844fb6f4fc0d13c6fa859b1e1308a8
hyvuz <haneennasser1425@outlook.com>
2025-08-27 20:07:54 +0300

    Create 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
+ */
+void print_alphabet(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	/* Print each character using _putchar */
+	for (i = 0; alphabet[i] != '\0'; i++)
+	{
+		_putchar(alphabet[i]);
+	}
+	_putchar('\n'); /* Print newline */
+}
